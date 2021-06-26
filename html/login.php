8e8f58e513f1ba7bce04e35e77334419d1b634b9
Abhishek Kagra <AbhishekKagra@aaddoo.net>
2021-06-26 14:17:58 +0530

    changes to the form

--- html/login.php
+++ html/login.php
@@ -9,16 +9,16 @@
   <body>
     <form class="container" action="" method="post">
       <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+        <label for="username">Username:</label>
+        <input type="text" class="form-control" placeholder="Enter Username" name="username">
       </div>
       <div class="form-group">
-        <label for="pwd">Password:</label>
-        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+        <label for="password">Password:</label>
+        <input type="password" class="form-control" placeholder="Enter password" name="password">
       </div>
       <div class="form-group form-check">
         <label class="form-check-label">
-          <input class="form-check-input" type="checkbox" name="remember"> Remember me
+          <input class="form-check-input" type="checkbox" name="remember"> Remember Me
         </label>
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
