54d6f13055f8a930456138f5870cbfe628b89f9e
Abhishek Kagra <AbhishekKagra@aaddoo.net>
2021-06-26 14:14:28 +0530

    changed action and method of the form

--- html/login.php
+++ html/login.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="../css/login.css">
   </head>
   <body>
-    <form class="container" action="/action_page.php">
+    <form class="container" action="" method="post">
       <div class="form-group">
         <label for="email">Email:</label>
         <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
