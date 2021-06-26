2862c93c5320af98a79f7b867743f2b39e3bf75e
Abhishek Kagra <AbhishekKagra@aaddoo.net>
2021-06-26 13:59:29 +0530

    added login page

--- html/login.php
+++ html/login.php
@@ -4,17 +4,13 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <!--Custom Css file-->
-    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/login.css">
   </head>
   <body>
-  <div class="head container-fluid navbar">
-  <a href="#" class="text-white display-4">AO</a>
-  <a href="html/login.php" class="btn btn-success float-right">Login</a>
-  </div>
-    <div class="background-1 container-fluid">
-    </div>
-    <div class="background-2 container-fluid">
-    </div>
+    <form class="form-group" action="" method="post">
+      <label for="username">Username</label>
+      <input type="text" name="username" placeholder="Username">
+    </form>
   </body>
   <!--bootstrapcdn JS links-->
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
