32e6530993ff658582ceb7418385fad544b25112
Abhishek Kagra <AbhishekKagra@aaddoo.net>
2021-07-11 16:42:13 +0530

    updated home page view for mobile view

--- html/home.php
+++ html/home.php
@@ -3,12 +3,25 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width , initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!--font links-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
     <!--Custom Css file-->
     <link rel="stylesheet" href="css/home.css">
   </head>
   <body>
-
+      <div class="navbar">
+        <p class="logo">NT</p>
+        <img class="hamburger" src="./assets/logo/hamburger.png" alt="Hamburger">
+      </div>
+          <div class="search-bar">
+          <input type="text" name="" placeholder="Search">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24.481" height="24.486" viewBox="0 0 24.481 24.486">
+          <path id="Icon_awesome-search" data-name="Icon awesome-search" d="M24.149,21.169,19.381,16.4a1.147,1.147,0,0,0-.813-.335h-.779a9.942,9.942,0,1,0-1.721,1.721v.779a1.147,1.147,0,0,0,.335.813l4.768,4.768a1.143,1.143,0,0,0,1.621,0L24.144,22.8a1.153,1.153,0,0,0,0-1.626Zm-14.2-5.1a6.121,6.121,0,1,1,6.121-6.121A6.117,6.117,0,0,1,9.946,16.067Z" fill="#707070"/>
+        </svg>
+      </div>
+    </div>
   </body>
   <!--bootstrapcdn JS links-->
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
